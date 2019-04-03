fface865ebd7ca87614db237353563f2f42cb919
UsuallyChris <schoonc@gmail.com>
2019-04-02 19:14:48 -0700

    Added author model

--- catalog/models.py
+++ catalog/models.py
@@ -39,7 +39,7 @@ class BookInstance(models.Model):
     ('r', 'Reserved'),
   )
 
-  status = mode.CharField(
+  status = models.CharField(
     max_length=1,
     choices=LOAN_STATUS,
     blank=True,
@@ -52,3 +52,18 @@ class BookInstance(models.Model):
 
   def __str__(self):
     return f'{self.id} ({self.book.title})'
+
+class Author(models.Model):
+  """Model representing an Author"""
+  first_name = models.CharField(max_length=100)
+  last_name = models.CharField(max_length=100)
+  date_of_birth = models.DateField(null=True, blank=True)
+
+  class Meta:
+    ordering = ['last_name', 'first_name']
+
+  def get_absolute_url(self):
+    return reverse('author-detail', args=[self.id])
+
+  def __str__(self):
+    return f'{self.last_name}, {self.first_name}'
