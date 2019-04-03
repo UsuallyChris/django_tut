797dd3606169a0e68becb032beaf711539559a2b
UsuallyChris <schoonc@gmail.com>
2019-04-02 18:33:03 -0700

    Added model for book genre.

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+  """Model representing a book genre."""
+  name = models.CharField(max_length=200, help_text='Enter a book genre.')
+
+  def __str__(self):
+    """String for representing the Model object."""
+    return self.name
