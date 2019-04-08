250ac27a046ca05c58bbbc68febef78ae533c8b0
UsuallyChris <schoonc@gmail.com>
2019-04-08 16:27:17 -0700

    Added ModelAdmin to Author

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,6 +4,10 @@ from catalog.models import Author, Genre, Book, BookInstance
 # Register your models here.
 
 admin.site.register(Book)
-admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(BookInstance)
+
+class AuthorAdmin(admin.ModelAdmin):
+  list_display = ('last_name', 'first_name', 'date_of_birth')
+
+admin.site.register(Author, AuthorAdmin)
